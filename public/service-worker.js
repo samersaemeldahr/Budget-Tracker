24587d6bef328a59b67bef295ef37d465aff4165
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-07-01 23:22:56 -0500

    Create cache file

---
+++ public/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'budget-cache-version-1';
+const DATA_CACHE_NAME = 'budget-data-cache-version-1';
+const FILES_TO_CACHE = [
+    '/',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png',
+    '/index.html',
+    '/index.js',
+    '/styles.css',
+    '/idb.js',
+    '/manifest.json'
+];
+
+// Fetch request
