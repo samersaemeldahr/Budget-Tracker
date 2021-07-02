9b7c80653c1b02131de04fb87a2fbfdf2820fa42
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-07-01 21:49:58 -0500

    Create object store

--- public/idb.js
+++ public/idb.js
@@ -1,3 +1,8 @@
 let db;
 const request = indexedDB.open('budget_tracker', 1);
 
+// Upgrade database
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore('transaction', { autoIncrement: true });
+};
