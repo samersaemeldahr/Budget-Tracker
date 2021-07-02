9b72e7b4e2ef7225dc56e6195e2012d85b5c3e8b
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-07-01 22:27:22 -0500

    Complete transaction

--- public/idb.js
+++ public/idb.js
@@ -23,3 +23,39 @@ function saveRecord(record) {
     const transactionObjectStore = transaction.objectStore('transaction')
     transactionObjectStore.add(record);
 };
+
+// Complete transaction
+function checkDatabase (){
+    const transaction = db.transaction(['ransaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('transaction');
+    const getAll = transactionObjectStore.getAll();
+
+
+    getAll.onsuccess = function(){
+        if(getAll.result.length > 0){
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response = response.json())
+            .then(serverResponse => {
+                if(serverResponse.message){
+                    throw new Error(serverResponse);
+                }
+
+                const transaction = db.transaction(['transaction', 'readwrite']);
+                const transactionObjectStore = transaction.objectStore('transaction');
+                transactionObjectStore.clear();
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    }
+}
+
+window.addEventListener('online', checkDatabase);
